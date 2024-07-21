ff603ce27182d72258c1b329213ebbdcd7efa7ad
jay koranga <korangajay17@gmail.com>
2024-07-21 18:57:34 +0530

    added local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import SideBar from "../components/SideBar";
 import Input from "../components/Input";
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState,useEffect } from "react";
 import Todo from "../components/Todo";
 import NewComponent from "../components/NewComponent";
 import NoProjectSelected from "../components/NoProjectSelected";
@@ -16,9 +16,33 @@ function App() {
   const [projects,setProjects]=useState([])
   const [showTopicMenue, setShowTopicMenue] = useState(true)
   const [projectHeading,setProjectHeading]=useState([]);
+  
+  useEffect(() => {
+    const storedProjectHeadings = window.localStorage.getItem('project-key');
+    if (storedProjectHeadings.length!=0) {
+      setProjectHeading(JSON.parse(storedProjectHeadings));
+    }
+  }, []);
+
+  useEffect(() => {
+    const storedTodoItems = window.localStorage.getItem('todo-key');
+    if (storedTodoItems.length!=0) {
+      setTodoItems(JSON.parse(storedTodoItems));
+    }
+  }, []);
+
+  useEffect(() => {
+    window.localStorage.setItem('project-key', JSON.stringify(projectHeading));
+  }, [projectHeading]);
+
+  useEffect(() => {
+    window.localStorage.setItem('todo-key', JSON.stringify(todoItems));
+  }, [todoItems]);
+
   function handleProjectClicks(index){
     console.log(`checking function index ${index} is clicked`)
     setClickedIndex(index);
+    if(showTopicMenue) setShowTopicMenue(false);
  }
   return (
     <MyContext.Provider value={{projects,setProjects,projectHeading,setProjectHeading,showTopicMenue, setShowTopicMenue,handleProjectClicks,clickedIndex,setClickedIndex,todoItems, setTodoItems}}>
